e5d07b9cbb42c3c4bec3fbaf0d864d6bca2e339a
juvvuj <juvvuj@gmail.com>
2020-10-01 09:55:18 -0400

    Add 'randomizer.randNumberInBetween' command
    - adds the `randomizer.randNumberInBetween' command to generate
    a random integer in between a user provided min and max value.
    
    Signed-off-by: juvvuj <juvvuj@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -15,6 +15,43 @@ export function activate(context: vscode.ExtensionContext) {
         })
     );
 
+    // Register the `randomizer.randNumberInBetween` command.
+    disposable.push(
+        vscode.commands.registerCommand(Commands.RANDOM_NUMBER_IN_BETWEEN, async () => {
+
+            const min = await vscode.window.showInputBox({
+                ignoreFocusOut: true,
+                placeHolder: 'Please provide a minimum number.',
+                prompt: 'Minimum Number',
+                validateInput: (value) => {
+                    const minimum = parseInt(value);
+                    if (typeof minimum !== 'number') {
+                        return undefined;
+                    }
+                }
+            });
+
+            const max = await vscode.window.showInputBox({
+                ignoreFocusOut: true,
+                placeHolder: 'Please provide a max number.',
+                prompt: 'Maximum Number',
+                validateInput: (value) => {
+                    const maximum = parseInt(value);
+                    if (typeof maximum !== 'number') {
+                        return undefined;
+                    }
+                }
+            });
+
+            if (!!min && !!max) {
+                const minNumber = parseInt(min);
+                const maxNumber = parseInt(max);
+                const number = Math.floor(Math.random() * maxNumber) + minNumber;
+                vscode.window.showInformationMessage(`Random Number: ${number}`);
+            }
+        })
+    );
+
     context.subscriptions.push(...disposable);
 }
 
--- src/types.ts
+++ src/types.ts
@@ -7,4 +7,9 @@ export namespace Commands {
      * Generates a random number.
      */
     export const RANDOM_NUMBER = 'randomizer.randNumber';
+    /**
+     * The `randNumberInBetween` command.
+     * Generates a random number in between two numbers provided by users.
+     */
+    export const RANDOM_NUMBER_IN_BETWEEN = 'randomizer.randNumberInBetween';
 }
